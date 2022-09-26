f500b2f012c00564800fea025f13603069cf4080
11notes <github@11notes.ch>
2022-09-26 15:01:47 +0200

    prism consensus layer

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,33 +1,23 @@
-#!/bin/ash
-if [ -z "$1" ]; then
-    set -- "geth" \
-        --datadir "/geth/var" \
-        --config "/geth/etc/config.toml"  \
-        --syncmode=snap \
-        --cache 65536  \
-        --txlookuplimit 0 \
-        --ws \
-            --ws.addr 0.0.0.0 \
-            --ws.api eth,web3,txpool \
-            --ws.origins '*' \
-        --http \
-            --http.addr 0.0.0.0 \
-            --http.api eth,web3,txpool \
-            --http.corsdomain '*' \
-        --authrpc.vhosts '*' \
-        --authrpc.jwtsecret /geth/etc/jwt
+#!/bin/bash
+geth \
+    --datadir "/eth/geth/var" \
+    --config "/eth/geth/etc/config.toml"  \
+    --syncmode=snap \
+    --cache 65536  \
+    --txlookuplimit 0 \
+    --ws \
+        --ws.addr 0.0.0.0 \
+        --ws.api eth,web3,txpool \
+        --ws.origins '*' \
+    --http \
+        --http.addr 0.0.0.0 \
+        --http.api eth,web3,txpool \
+        --http.corsdomain '*' \
+    --authrpc.addr 0.0.0.0 \
+    --authrpc.vhosts '*' \
+    --authrpc.jwtsecret /eth/geth/etc/jwt &
 
-
-    exec "$@"
-else
-    case $1 in
-        "prune")
-            set -- "geth" \
-                snapshot \
-                prune-state \
-                --datadir "/geth/var"
-
-            exec "$@"
-        ;;
-    esac
-fi
+PRYSM_ALLOW_UNVERIFIED_BINARIES=1 /eth/prysm/bin/prysm beacon-chain \
+    --datadir="/eth/prysm/var"\
+    --execution-endpoint=http://localhost:8551 \
+    --jwt-secret=/eth/geth/etc/jwt --accept-terms-of-use
